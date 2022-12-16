98e7f91e0622942e660740b740d0d31684b716e4
Joymae Tajos <120698931+joymaetajos@users.noreply.github.com>
2022-12-16 11:51:08 +0800

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
