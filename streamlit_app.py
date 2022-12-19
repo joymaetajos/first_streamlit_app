c962f99d71f2e900b28bd4576d516196c2f0475c
Joymae Tajos <120698931+joymaetajos@users.noreply.github.com>
2022-12-19 11:28:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,19 +66,37 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 
 #new section to to display furityvice api response
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#       streamlit.error ("Please select a fruit to get information.")
+#   else:
+#     back_from_function = get_fruityvice_data(fruit_choice)
+#      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#     streamlit.dataframe(back_from_function)
+#      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())                                
+#except URLError as e:
+#      streamlit.error()
+# import requests
+
+##create the repeatable block
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())                                
+      return fruityvice_normalized
+      
+#new section to to display furityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-       streamlit.error ("Please select a fruit to get information.")
+        streamlit.error ("Please select a fruit to get information.")
    else:
-#     back_from_function = get_fruityvice_data(fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#     streamlit.dataframe(back_from_function)
-      fruity_normalized = pandas.json_normalize(fruityvice_response.json())                                
-except URLError as e:
-      streamlit.error()
-# import requests
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+
+
 
 #streamlit.write('The user entered ', fruit_choice)
 
