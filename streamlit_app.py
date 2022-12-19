3307e14c174b87104aa7bb95b0bbd40fde87b68e
Joymae Tajos <120698931+joymaetajos@users.noreply.github.com>
2022-12-19 11:13:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruitload list contains")
 streamlit.dataframe(my_data_rows)
 
-
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 
 
