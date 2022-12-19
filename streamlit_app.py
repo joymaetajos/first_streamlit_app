dbdb01996c22630319b3641ecb4cbaa8112fc5a8
Joymae Tajos <120698931+joymaetajos@users.noreply.github.com>
2022-12-19 08:19:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,5 @@ streamlit.dataframe(my_data_rows)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('The user entered ', add_my_fruit)
+  streamlit.write('Thanks for adding ', add_my_fruit)
 
